6c3ee632b69ede53035e42cf37fe8e0355d229e1
Nihal Ismail <Nihal Ismail>
2016-11-15 23:16:37 +0530

    Fired up the skelton

---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    void Start()
+    {
+        //Load main menu if current level is splash screen
+        if(isSplash())
+        {
+            loadMainMenu();
+        }
+    }
+
+    public void loadLevel(string levelName)
+    {
+        Application.LoadLevel(levelName);
+    }
+
+
+    void loadMainMenu()
+    {
+        Invoke("loadNextLevel", 5f);
+    }
+
+    void loadNextLevel()
+    {
+        
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    bool isSplash()
+    {
+        //Level 0->00_SPLASH
+        if (Application.loadedLevel == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/_Scripts/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+    [Tooltip("Music clips played through each levels")]
+    public AudioClip[] gameMusicArray;
+    AudioSource musicPlayer;
+    int musicIndex;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+        musicPlayer = GetComponent<AudioSource>();
+	}
+	
+    void OnLevelWasLoaded(int level)
+    {
+        musicIndex = level - 1;
+        musicPlayer.clip = gameMusicArray[musicIndex];
+        if (musicPlayer.clip)
+        {
+            musicPlayer.loop = true;
+            musicPlayer.playOnAwake = true;
+            musicPlayer.Play();
+        }
+        else
+        {
+            Debug.LogError("Clip not founded");
+        }
+    }
+}
