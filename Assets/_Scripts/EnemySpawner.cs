1c60b9d3e7b1e7187bed6b6aded5cff6c93eefd4
Nihal Ismail <Nihal Ismail>
2016-11-16 00:35:26 +0530

    Added enemy spawner at 0,0,0

---
+++ Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyFormationPrefab;
+    GameObject enemyFormation;
+    float probability, spawnRate, formationVelocity;
+    Rigidbody2D formationRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        spawnRate = 0.1f;
+        formationVelocity = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        probability = spawnRate * Time.deltaTime;
+        if(probability>Random.value)
+        {
+            enemyFormation=Instantiate(enemyFormationPrefab,transform.position,Quaternion.identity) as GameObject;
+            formationRigidBody = enemyFormation.GetComponent<Rigidbody2D>();
+            formationRigidBody.velocity = Vector3.down*formationVelocity;
+        }
+	}
+
+    void OnDrawGizmos()
+    {
+        Debug.Log("Draw gizoms");
+        Gizmos.DrawWireCube(transform.position,Vector3.one);
+    }
+}
