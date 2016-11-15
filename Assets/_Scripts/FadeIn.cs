761d7613ee6ac38686a6676258ea9f91d3e6addc
Nihal Ismail <Nihal Ismail>
2016-11-15 23:39:49 +0530

    Alpha 0.0001 realease

---
+++ Assets/_Scripts/FadeIn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+
+    Color panelImageColor;
+    Image panelImage;
+    float fadeInTime;
+
+	// Use this for initialization
+	void Start () {
+        //Smaller the value,Higher the loading time :P
+        fadeInTime = 0.75f;
+        panelImage = GetComponent<Image>();
+        panelImageColor = panelImage.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        panelImageColor.a-=fadeInTime*Time.deltaTime;
+        panelImage.color=panelImageColor;
+	}
+}
