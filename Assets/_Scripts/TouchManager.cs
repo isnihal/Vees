2a8eee68e3d5a8ba94124417524d431618e68061
Nihal Ismail <Nihal Ismail>
2016-11-16 22:37:48 +0530

    Code refactoring

---
+++ Assets/_Scripts/TouchManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour {
+
+    static Vector3 startPositon, endPosition;
+    static float startTime, endTime,xVelocity;
+
+    void DragStart()
+    {
+        startPositon = Input.mousePosition;
+        startTime = Time.time;
+    }
+
+    void DragEnd()
+    {
+        endPosition = Input.mousePosition;
+        endTime = Time.time;
+
+        xVelocity = (endPosition.x - startPositon.x) / (endTime - startTime);
+    }
+
+    public static float getXvelocity()
+    {
+        return xVelocity;
+    }
+
+    public static float getYpos()
+    {
+        return (startPositon.y);
+    }
+}
