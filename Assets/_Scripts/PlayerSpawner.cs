bc2a9d5e6a54d8e106b24086a7137055c5126a7b
Nihal Ismail <Nihal Ismail>
2016-11-16 23:22:10 +0530

    Inner feeling to commit

--- Assets/_Scripts/PlayerSpawner.cs
+++ Assets/_Scripts/PlayerSpawner.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class PlayerSpawner : MonoBehaviour {
 
     public GameObject playerFormation;
+    GameObject spawnedPlayer;
+    Rigidbody2D spawnedPlayerRigidBody;
 
-    public void spawnPlayer(Vector3 spawningPosition)
+    public void spawnPlayer(Vector3 spawningPosition,Vector3 playerVelocity)
     {
-        Instantiate(playerFormation,spawningPosition, Quaternion.identity);
+        spawningPosition.x = transform.position.x;
+        spawnedPlayer = Instantiate(playerFormation, spawningPosition, Quaternion.Euler(new Vector3(0,0,90))) as GameObject;
+        spawnedPlayerRigidBody = spawnedPlayer.GetComponent<Rigidbody2D>();
+        spawnedPlayerRigidBody.velocity = playerVelocity;
     }
 }
--- Assets/_Scripts/TouchManager.cs
+++ Assets/_Scripts/TouchManager.cs
@@ -5,28 +5,32 @@ public class TouchManager : MonoBehaviour {
 
     static Vector3 startPositon, endPosition;
     static float startTime, endTime,xVelocity;
+    PlayerSpawner playerSpawner;
 
     void DragStart()
     {
         startPositon = Input.mousePosition;
         startTime = Time.time;
+        playerSpawner = FindObjectOfType<PlayerSpawner>();
     }
 
     void DragEnd()
     {
         endPosition = Input.mousePosition;
         endTime = Time.time;
-
+        
+        //Spawn a player formation start
+        //Calculate X velocity
         xVelocity = (endPosition.x - startPositon.x) / (endTime - startTime);
-    }
+        xVelocity /= 10;
 
-    public static float getXvelocity()
-    {
-        return xVelocity;
-    }
+        //Vector refactoring
+        Vector3 playerPosition = new Vector3(0, 0, 0);
+        Debug.Log("Y pos" + startPositon.y);
+        Vector3 playerVelocity = new Vector3(xVelocity, 0, 0);
+        //Spawn player after swipe
 
-    public static float getYpos()
-    {
-        return (startPositon.y);
+        playerSpawner.spawnPlayer(playerPosition,playerVelocity);
+        //Spawn a player formation end
     }
 }
