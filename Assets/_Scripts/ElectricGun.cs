a59b5a2d53e12dfc443a8ebec109217a3214a3d7
Nihal Ismail <Nihal Ismail>
2016-11-30 15:41:33 +0530

    Minimal gun working

--- Assets/_Scripts/ElectricGun.cs
+++ Assets/_Scripts/ElectricGun.cs
@@ -5,15 +5,27 @@ public class ElectricGun : MonoBehaviour {
 
     //Electric gun effect code
     static int charge;
+    float currentTime, lastIncrementedTime, timeToCharge;
 
 	// Use this for initialization
 	void Start () {
         charge = 10;
-	}
+        currentTime = 0;
+        lastIncrementedTime = 0;
+        timeToCharge = 5;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        currentTime = Time.time;
+        if((currentTime-lastIncrementedTime) >=timeToCharge)
+        {
+            //Increase charge
+            incrementCharge();
+            lastIncrementedTime = currentTime;
+            Debug.Log("Incrementing charge at "+currentTime);
+        }
+
 	}
 
     public static int getCharge()
