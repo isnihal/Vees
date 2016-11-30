e75b6302af6865d59609d530559bb8e29e304728
Nihal Ismail <Nihal Ismail>
2016-11-30 20:41:04 +0530

    AD Manager added

---
+++ Assets/_Scripts/AdManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class AdManager : MonoBehaviour {
+
+    public void showAd()
+    {
+        Advertisement.Show();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
