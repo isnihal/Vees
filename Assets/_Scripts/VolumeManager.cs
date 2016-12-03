c7e9a8bba922da7f6d2fe69b4b09bfc838c3d1c3
Nihal Ismail <Nihal Ismail>
2016-12-03 13:31:01 +0530

    Added a new mute button

---
+++ Assets/_Scripts/VolumeManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeManager : MonoBehaviour {
+
+    bool isMuted;
+
+    void Start()
+    {
+        isMuted = false;
+    }
+
+    public void muteVolume()
+    {
+        if (!isMuted)
+        {
+            MusicPlayer.setVolume(0);
+            isMuted = true;
+        }
+        else
+        {
+            MusicPlayer.setVolume(1);
+            isMuted = false;
+        }
+    }
+}
