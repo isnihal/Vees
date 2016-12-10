482c1a6e4982cfae68d00dd924d2388f21a49e7e
Nihal Ismail <Nihal Ismail>
2016-12-10 22:45:57 +0530

    Stinits again with a mute button

---
+++ Assets/VolumeManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VolumeManager : MonoBehaviour {
+
+    static bool isMuted=false;
+    public Sprite muteIcon, unMuteIcon;
+    public GameObject muteButton;
+    
+
+    void Start()
+    {
+        if(isMuted)
+        {
+            muteButton.GetComponent<Image>().sprite = muteIcon;
+        }
+
+        else
+        {
+            muteButton.GetComponent<Image>().sprite = unMuteIcon;
+        }
+    }
+    public void muteGame()
+    {
+        if(isMuted)
+        {
+            isMuted = false;
+            muteButton.GetComponent<Image>().sprite = unMuteIcon;
+            MusicPlayer.setVolume(0.5f);
+        }
+
+        else
+        {
+            isMuted = true;
+            muteButton.GetComponent<Image>().sprite = muteIcon;
+            MusicPlayer.setVolume(0f);
+        }
+    }
+
+    public static bool getIsMuted()
+    {
+        return isMuted;
+    }
+}
