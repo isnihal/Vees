64dd0ffcd3510fa1ac4322eb8409c2a95710cc1f
Nihal Ismail <Nihal Ismail>
2016-12-20 14:08:41 +0530

    IAP Manager added

---
+++ Assets/_Scripts/IAPManager.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+
+public class Purchaser : MonoBehaviour, IStoreListener
+{
+    private static IStoreController m_StoreController;          // The Unity Purchasing system.
+    private static IExtensionProvider m_StoreExtensionProvider; // The store-specific Purchasing subsystems.
+
+
+    public static string oneBeer = "beer1";
+    public static string twoBeer = "beer2";
+    public static string threeBeer = "beer3";
+    public static string fourBeer = "beer4";
+    public static string fiveBeer = "beer5";
+
+    static bool hasUserPurchased;
+
+
+    // Google Play Store-specific product identifier subscription product.
+    private static string kProductNameGooglePlaySubscription = "com.truffleandcaffeine.vees";
+
+    void Start()
+    {
+        // If we haven't set up the Unity Purchasing reference
+        if (m_StoreController == null)
+        {
+            // Begin to configure our connection to Purchasing
+            InitializePurchasing();
+        }
+
+        hasUserPurchased = false;
+    }
+
+    public void InitializePurchasing()
+    {
+        // If we have already connected to Purchasing ...
+        if (IsInitialized())
+        {
+            // ... we are done here.
+            return;
+        }
+
+        // Create a builder, first passing in a suite of Unity provided stores.
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+
+        //TODO:Add all IAP Products here
+        builder.AddProduct(oneBeer, ProductType.NonConsumable);
+        builder.AddProduct(twoBeer, ProductType.NonConsumable);
+        builder.AddProduct(threeBeer, ProductType.NonConsumable);
+        builder.AddProduct(fourBeer, ProductType.NonConsumable);
+        builder.AddProduct(fiveBeer, ProductType.NonConsumable);
+
+
+        // Kick off the remainder of the set-up with an asynchrounous call, passing the configuration 
+        // and this class' instance. Expect a response either in OnInitialized or OnInitializeFailed.
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+
+    private bool IsInitialized()
+    {
+        // Only say we are initialized if both the Purchasing references are set.
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+
+    public void BuyNonConsumable(string productId)
+    {
+        // Buy the non-consumable product using its general identifier. Expect a response either 
+        // through ProcessPurchase or OnPurchaseFailed asynchronously.
+        switch (productId)
+        {
+            case "beer1":
+                BuyProductID(oneBeer);
+                break;
+            case "beer2":
+                BuyProductID(twoBeer);
+                break;
+            case "beer3":
+                BuyProductID(threeBeer);
+                break;
+            case "beer4":
+                BuyProductID(fourBeer);
+                break;
+            case "beer5":
+                BuyProductID(fiveBeer);
+                break;
+        }
+    }
+
+    void BuyProductID(string productId)
+    {
+        // If Purchasing has been initialized ...
+        if (IsInitialized())
+        {
+            // ... look up the Product reference with the general product identifier and the Purchasing 
+            // system's products collection.
+            Product product = m_StoreController.products.WithID(productId);
+
+            // If the look up found a product for this device's store and that product is ready to be sold ... 
+            if (product != null && product.availableToPurchase)
+            {
+                Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+                // ... buy the product. Expect a response either through ProcessPurchase or OnPurchaseFailed 
+                // asynchronously.
+                m_StoreController.InitiatePurchase(product);
+            }
+            // Otherwise ...
+            else
+            {
+                // ... report the product look-up failure situation  
+                Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+            }
+        }
+        // Otherwise ...
+        else
+        {
+            // ... report the fact Purchasing has not succeeded initializing yet. Consider waiting longer or 
+            // retrying initiailization.
+            Debug.Log("BuyProductID FAIL. Not initialized.");
+        }
+    }
+
+
+    // Restore purchases previously made by this customer. Some platforms automatically restore purchases, like Google. 
+    // Apple currently requires explicit purchase restoration for IAP, conditionally displaying a password prompt.
+    public void RestorePurchases()
+    {
+        // If Purchasing has not yet been set up ...
+        if (!IsInitialized())
+        {
+            // ... report the situation and stop restoring. Consider either waiting longer, or retrying initialization.
+            Debug.Log("RestorePurchases FAIL. Not initialized.");
+            return;
+        }
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        // Purchasing has succeeded initializing. Collect our Purchasing references.
+        Debug.Log("OnInitialized: PASS");
+
+        // Overall Purchasing system, configured with products for this application.
+        m_StoreController = controller;
+        // Store specific subsystem, for accessing device-specific store features.
+        m_StoreExtensionProvider = extensions;
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        // Purchasing set-up has not succeeded. Check error for reason. Consider sharing this reason with the user.
+        Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        if (String.Equals(args.purchasedProduct.definition.id, oneBeer, StringComparison.Ordinal))
+        {
+            Debug.Log("Alert: 1 Beer Purchased");
+            hasUserPurchased = true;
+        }
+
+        else if (String.Equals(args.purchasedProduct.definition.id, twoBeer, StringComparison.Ordinal))
+        {
+            Debug.Log("Alert: 2 Beer Purchased");
+            hasUserPurchased = true;
+        }
+
+        else if (String.Equals(args.purchasedProduct.definition.id, threeBeer, StringComparison.Ordinal))
+        {
+            Debug.Log("Alert: 3 Beer Purchased");
+            hasUserPurchased = true;
+        }
+
+        else if (String.Equals(args.purchasedProduct.definition.id, fourBeer, StringComparison.Ordinal))
+        {
+            Debug.Log("Alert: 4 Beer Purchased");
+            hasUserPurchased = true;
+        }
+
+        else if (String.Equals(args.purchasedProduct.definition.id, fiveBeer, StringComparison.Ordinal))
+        {
+            Debug.Log("Alert: 5 Beer Purchased");
+            hasUserPurchased = true;
+        }
+
+        else
+        {
+            Debug.Log("IAP Purchase fail");
+        }
+
+        // Return a flag indicating whether this product has completely been received, or if the application needs 
+        // to be reminded of this purchase at next app launch. Use PurchaseProcessingResult.Pending when still 
+        // saving purchased products to the cloud, and when that save is delayed. 
+        return PurchaseProcessingResult.Complete;
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        // A product purchase attempt did not succeed. Check failureReason for more detail. Consider sharing 
+        // this reason with the user to guide their troubleshooting actions.
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+    }
+}
