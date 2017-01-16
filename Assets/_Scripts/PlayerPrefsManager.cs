96080b0257674e6eecee77a73dbca28075d8d07a
Nihal Ismail <Nihal Ismail>
2017-01-16 20:13:05 +0530

    More code

--- Assets/_Scripts/PlayerPrefsManager.cs
+++ Assets/_Scripts/PlayerPrefsManager.cs
@@ -3,17 +3,17 @@ using System.Collections;
 
 public class PlayerPrefsManager : MonoBehaviour {
 
-    static string LANGUAGE_KEY,MUTE_KEY,FIRST_TIME_KEY,EQUALS_HIGH,ONE_HIGH,ESCAPE_HIGH,LAPSE_HIGH;
+    static string LANGUAGE_KEY, MUTE_KEY, FIRST_TIME_KEY,ONE_FIRST,EQUAL_FIRST,ESCAPE_FIRST,LAPSE_FIRST;
 
     void Start()
     {
         LANGUAGE_KEY = "LANGUAGE";
         MUTE_KEY = "MUTE";
         FIRST_TIME_KEY = "FIRST_TIME";
-        ONE_HIGH = "ONE_HIGH";
-        EQUALS_HIGH = "EQUALS_HIGH";
-        ESCAPE_HIGH = "ESCAPE_HIGH";
-        LAPSE_HIGH = "LAPSE_HIGH";
+        ONE_FIRST = "ONE_FIRST";
+        EQUAL_FIRST = "EQUAL_FIRST";
+        ESCAPE_FIRST = "ESCAPE_FIRST";
+        LAPSE_FIRST = "LAPSE_FIRST";
     }
 
     public static void setLanguage(string selectedLanguage)
@@ -73,124 +73,124 @@ public class PlayerPrefsManager : MonoBehaviour {
         }
     }
 
-    /*public static int getHighScore(int fromLevel)
+
+    public static void setOneDirectionFirstTime()
     {
-        switch(fromLevel)
-        {
-            //One Way
-            case 3:
-                if (PlayerPrefs.HasKey(ONE_HIGH))
-                {
-                    return (PlayerPrefs.GetInt(ONE_HIGH));
-                }
-                else
-                {
-                    return 99;
-                }
-            //Escape
-            case 5:
-                if (PlayerPrefs.HasKey(ESCAPE_HIGH))
-                {
-                    return (PlayerPrefs.GetInt(ESCAPE_HIGH));
-                }
-                else
-                {
-                    return 99;
-                }
-            //Equals
-            case 6: if (PlayerPrefs.HasKey(EQUALS_HIGH))
-                {
-                    return (PlayerPrefs.GetInt(EQUALS_HIGH));
-                }
-                else
-                {
-                    return 99;
-                }
-            //Lapse
-            case 7: if (PlayerPrefs.HasKey(LAPSE_HIGH))
-                {
-                    return (PlayerPrefs.GetInt(LAPSE_HIGH));
-                }
-                else
-                {
-                    return 99;
-                }
-
-            default:return (999);
-        }
-    }
-
-
-    public static void saveHighScore(int score,int fromLevel)
-    {
-        switch (fromLevel)
-        {
-            //One Way
-            case 3:
-                if(score>PlayerPrefs.GetInt(ONE_HIGH))
-                {
-                    PlayerPrefs.SetInt(ONE_HIGH, score);
-                }
-                break; 
-            //Escape
-            case 5:
-                if (score > PlayerPrefs.GetInt(ESCAPE_HIGH))
-                {
-                    PlayerPrefs.SetInt(ESCAPE_HIGH, score);
-                }
-                break;
-            //Equals
-            case 6:
-                if (score > PlayerPrefs.GetInt(EQUALS_HIGH))
-                {
-                    PlayerPrefs.SetInt(EQUALS_HIGH, score);
-                }
-                break;
-            //Lapse
-            case 7:
-                if (score > PlayerPrefs.GetInt(LAPSE_HIGH))
-                {
-                    PlayerPrefs.SetInt(LAPSE_HIGH, score);
-                }
-                break;
-        }
+        PlayerPrefs.SetInt(ONE_FIRST, 1);
         PlayerPrefs.Save();
     }
 
-    public static bool isHighScore(int score, int fromLevel)
+    public static bool isOneDirectionFirstTime()
     {
-        switch (fromLevel)
+        if (PlayerPrefs.GetInt(ONE_FIRST) != 1)
         {
-            //One Way
-            case 3:
-                if (score > PlayerPrefs.GetInt(ONE_HIGH))
-                {
-                    return true;
-                }
-                break;
-            //Escape
-            case 5:
-                if (score > PlayerPrefs.GetInt(ESCAPE_HIGH))
-                {
-                    return true;
-                }
-                break;
-            //Equals
-            case 6:
-                if (score > PlayerPrefs.GetInt(EQUALS_HIGH))
-                {
-                    return true;
-                }
-                break;
-            //Lapse
-            case 7:
-                if (score > PlayerPrefs.GetInt(LAPSE_HIGH))
-                {
-                    return true;
-                }
-                break;
-            default:return false;
+            return true;
         }
-        return false;
-    }*/
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool hasOneDirectionFirstTimeKey()
+    {
+        if (PlayerPrefs.HasKey(ONE_FIRST))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void setEqualsFirstTime()
+    {
+        PlayerPrefs.SetInt(EQUAL_FIRST, 1);
+        PlayerPrefs.Save();
+    }
+
+    public static bool isEqualsFirstTime()
+    {
+        if (PlayerPrefs.GetInt(EQUAL_FIRST) != 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool hasEqualsFirstTimeKey()
+    {
+        if (PlayerPrefs.HasKey(EQUAL_FIRST))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void setEscapeFirstTime()
+    {
+        PlayerPrefs.SetInt(ESCAPE_FIRST, 1);
+        PlayerPrefs.Save();
+    }
+
+    public static bool isEscapeFirstTime()
+    {
+        if (PlayerPrefs.GetInt(ESCAPE_FIRST) != 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool hasEscapeFirstTimeKey()
+    {
+        if (PlayerPrefs.HasKey(ESCAPE_FIRST))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void setLapseFirstTime()
+    {
+        PlayerPrefs.SetInt(LAPSE_FIRST, 1);
+        PlayerPrefs.Save();
+    }
+
+    public static bool isLapseFirstTime()
+    {
+        if (PlayerPrefs.GetInt(LAPSE_FIRST) != 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool hasLapseFirstTimeKey()
+    {
+        if (PlayerPrefs.HasKey(LAPSE_FIRST))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
